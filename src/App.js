3ad2e446330633927801196bcfc55d5bca8911c5
laclong290101 <73872396+laclong290101@users.noreply.github.com>
2021-10-14 22:35:35 +0700

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './assets/sass/main.scss'
 import Routes from './router/Router'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Routes/>
+      <Routes />
     </div>
   );
 }
